ea7e870382685012fda1f67b3385d2dfc82d4ce8
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2024-01-08 14:09:45 +0100

    created c file

--- src/compiler/m_compile.py
+++ src/compiler/m_compile.py
@@ -4,5 +4,15 @@
 
 def compile_source():
 
+    with open("hello_world.ll", "r", encoding="utf-8") as file_ll:
+        text_ll = file_ll \
+            .read()
+
+    text_c = ""
+
+    with open("hello_world.c", "w", encoding="utf-8") as file_c:
+        file_c \
+            .write(text_c)
+
     return None
 
