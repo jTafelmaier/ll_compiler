ce4609d25b7017eebcb00dafd15a205432a9bb4f
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2024-01-13 19:19:59 +0100

    renamed files

--- compiler_to_python.py
+++ compiler_to_python.py
@@ -7,11 +7,11 @@ from src.compiler import m_compile
 
 def main():
 
-    with open("hello_world.ll", "r", encoding="utf-8") as file_ll:
+    with open("example.ll", "r", encoding="utf-8") as file_ll:
         text_ll = file_ll \
             .read()
 
-    with open("hello_world.py", "w", encoding="utf-8") as file_python:
+    with open("example.py", "w", encoding="utf-8") as file_python:
         file_python \
             .write(m_compile.get_text_python_main(text_ll))
 
