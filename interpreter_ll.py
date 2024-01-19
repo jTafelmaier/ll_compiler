da33f49baf9a473c4dfe3dc7ca2b5a7dd816d9c9
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2024-01-19 17:32:02 +0100

    added interpreter script

---
+++ interpreter_ll.py
@@ -0,0 +1,21 @@
+
+
+import compiler_ll_to_python
+
+
+
+
+def main():
+
+    compiler_ll_to_python.main()
+
+    import example
+
+    example.main()
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
+
