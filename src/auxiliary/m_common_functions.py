65dab3b0fc1c7d5cd1b0cdd8cec5b23ff2c8e9fa
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2024-01-21 15:32:53 +0100

    refactored

--- src/auxiliary/m_common_functions.py
+++ src/auxiliary/m_common_functions.py
@@ -22,56 +22,18 @@ def get_text_indented_one_level(
                     .split("\n")))
 
 
-def get_text_unindented_one_level(
-    text:str):
-
-    def get_text_line_unindented(
-        text_line:str):
-
-        if text_line == "":
-            return text_line
-
-        assert text_line.startswith("    ")
-
-        return text_line \
-            [4:]
-
-    return "\n" \
-        .join(
-            map(
-                get_text_line_unindented,
-                text \
-                    .split("\n")))
-
-
 def get_iterator_texts_grouped_by_and_remove_indentation(
     text:str):
 
-    def get_text_unindent_lines_except_first(
-        text:str):
-
-        text_first_line, \
-        text_newline_partition, \
-        text_remaining_indented = text \
-            .partition("\n")
-
-        if text_newline_partition == "":
-            return text
-
-        text_remaining_unindented = get_text_unindented_one_level(text_remaining_indented)
-
-        return text_first_line \
-            + "\n" \
-            + text_remaining_unindented
-
     def get_iterator_reversed_texts_grouped_by_indentation():
 
-        list_texts_lines = text \
-            .split("\n")
+        iterator_reversed_texts_lines = reversed(
+                text \
+                    .split("\n"))
 
         text_block = ""
 
-        for text_line in reversed(list_texts_lines):
+        for text_line in iterator_reversed_texts_lines:
             if text_block != "":
                 text_block = "\n" \
                     + text_block
@@ -79,17 +41,17 @@ def get_iterator_texts_grouped_by_and_remove_indentation(
             if text_line == "":
                 continue
 
-            text_block = text_line \
-                + text_block
-
-            if not text_line.startswith("    "):
-                yield text_block
+            if text_line.startswith("    "):
+                text_block = text_line \
+                    [4:] \
+                    + text_block
+            else:
+                yield text_line \
+                    + text_block
 
                 text_block = ""
 
-    return map(
-            get_text_unindent_lines_except_first,
-            reversed(
-                list(
-                    get_iterator_reversed_texts_grouped_by_indentation())))
+    return reversed(
+            list(
+                get_iterator_reversed_texts_grouped_by_indentation()))
 
