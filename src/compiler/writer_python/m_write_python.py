a6fffeecce71820dab7aa2fbe258885c05e244f1
Johannes Stephan Tafelmaier <johannes@tafelmaier-bammer.de>
2024-01-21 23:47:18 +0100

    refactored

--- src/compiler/writer_python/m_write_python.py
+++ src/compiler/writer_python/m_write_python.py
@@ -13,158 +13,154 @@ TEXT_PREFIX_TO_AVOID_NAME_CLASHES = "nonpython_"
 TEXT_INPUT = TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
     + "Input"
 
-def get_text_python_function_call(
-    text_input:str,
-    dict_function:typing.Dict):
-
-    text_name_function = dict_function \
-        [m_shared.Function_reference.KEY_NAME_FUNCTION]
-
-    list_dicts_arguments = dict_function \
-        [m_shared.Function_reference.KEY_ARRAY_OBJECTS_ARGUMENTS]
-
-    list_texts_arguments_additional = list(
-            map(
-                get_text_python_expression,
-                list_dicts_arguments))
-
-    text_arguments_python = ",\n" \
-        .join([
-                text_input] \
-            + list_texts_arguments_additional)
-
-    return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
-        + text_name_function \
-        + "(\n" \
-        + m_common_functions.get_text_indented_one_level(text_arguments_python) \
-        + ")"
-
-
-def get_text_python_def(
-    dict_def:typing.Dict):
-
-    text_name_function = dict_def \
-        [m_shared.Function_definition.KEY_TEXT_NAME_FUNCTION]
-
-    # text_type_input = dict_def \
-    #     [m_shared.Function_definition.KEY_TEXT_TYPE_INPUT]
-
-    list_dicts_arguments = dict_def \
-        [m_shared.Function_definition.KEY_ARRAY_OBJECTS_ARGUMENTS]
-
-    # TODO ensure at least one return
-    list_dicts_body = dict_def \
-        [m_shared.Function_definition.KEY_ARRAY_OBJECTS_BODY]
-
-    def get_text_arguments():
-
-        def get_text_argument(
-            dict_argument:typing.Dict):
-
-            return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
-                + dict_argument \
-                    [m_shared.Function_definition.Argument.KEY_TEXT_NAME]
-
-        return ",\n" \
-            .join(
-                [TEXT_INPUT] \
-                    + list(
-                        map(
-                            get_text_argument,
-                            list_dicts_arguments)))
-
-    text_body = get_text_arguments() \
-        + "):\n\n" \
-        + get_text_python(list_dicts_body)
-
-    return "def " \
-        + TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
-        + text_name_function \
-        + "(\n" \
-        + m_common_functions.get_text_indented_one_level(text_body)
-
-
-def get_text_python_memory_allocation(
-    dict_memory_allocation:typing.Dict):
-
-    text_key_memory = dict_memory_allocation \
-        [m_shared.Memory_allocation.KEY_TEXT_KEY_MEMORY]
-
-    dict_expression = dict_memory_allocation \
-        [m_shared.Memory_allocation.KEY_OBJECT_CONTENT]
-
-    return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
-        + text_key_memory \
-        + " = " \
-        + get_text_python_expression(dict_expression)
-
-
-def get_text_python_return(
-    dict_return:typing.Dict):
-
-    return "return " \
-        + get_text_python_expression(
-                dict_return 
-                    [m_shared.Expression_return.KEY_OBJECT])
-
-
-def get_text_python_expression(
-    dict_expression:typing.Dict):
-
-    def get_text_literal(
-        dict_literal:typing.Dict):
-
-        return dict_literal \
-            [m_shared.Literal.KEY_TEXT_VALUE]
-
-    def get_text_memory_read(
-        dict_memory_read:typing.Dict):
-
-        return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
-            + dict_memory_read \
-                [m_shared.Memory_read.KEY_TEXT_KEY_MEMORY]
-
-    def get_text_function(
-        dict_function:typing.Dict):
-
-        return "lambda " \
-            + TEXT_INPUT \
-            + ": " \
-            + get_text_python_function_call(
-                    text_input=TEXT_INPUT,
-                    dict_function=dict_function)
-
-    def get_text_initial(
-        dict_initial:typing.Dict):
-
-        text_category = dict_initial \
-            [m_shared.Object_variable.KEY_TEXT_CATEGORY]
-
-        return {
-            "literal": get_text_literal,
-            "memory_read": get_text_memory_read,
-            "function": get_text_function} \
-            [text_category] \
-            (dict_initial)
-
-    dict_initial = dict_expression \
-        [m_shared.Expression.KEY_OBJECT_INITIAL]
-
-    list_dicts_function_calls = dict_expression \
-        [m_shared.Expression.KEY_ARRAY_OBJECTS_FUNCTION_CALLS]
-
-    text_python_current = get_text_initial(dict_initial)
-
-    for dict_function in list_dicts_function_calls:
-        text_python_current = get_text_python_function_call(
-                text_input=text_python_current,
-                dict_function=dict_function)
-
-    return text_python_current
-
 
 def get_text_python_block(
     dict_block:typing.Dict):
+
+    def get_text_python_function_call(
+        text_input:str,
+        dict_function:typing.Dict):
+
+        text_name_function = dict_function \
+            [m_shared.Function_reference.KEY_NAME_FUNCTION]
+
+        list_dicts_arguments = dict_function \
+            [m_shared.Function_reference.KEY_ARRAY_OBJECTS_ARGUMENTS]
+
+        list_texts_arguments_additional = list(
+                map(
+                    get_text_python_expression,
+                    list_dicts_arguments))
+
+        text_arguments_python = ",\n" \
+            .join([
+                    text_input] \
+                + list_texts_arguments_additional)
+
+        return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
+            + text_name_function \
+            + "(\n" \
+            + m_common_functions.get_text_indented_one_level(text_arguments_python) \
+            + ")"
+
+    def get_text_python_def(
+        dict_def:typing.Dict):
+
+        text_name_function = dict_def \
+            [m_shared.Function_definition.KEY_TEXT_NAME_FUNCTION]
+
+        # text_type_input = dict_def \
+        #     [m_shared.Function_definition.KEY_TEXT_TYPE_INPUT]
+
+        list_dicts_arguments = dict_def \
+            [m_shared.Function_definition.KEY_ARRAY_OBJECTS_ARGUMENTS]
+
+        # TODO ensure at least one return
+        list_dicts_body = dict_def \
+            [m_shared.Function_definition.KEY_ARRAY_OBJECTS_BODY]
+
+        def get_text_arguments():
+
+            def get_text_argument(
+                dict_argument:typing.Dict):
+
+                return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
+                    + dict_argument \
+                        [m_shared.Function_definition.Argument.KEY_TEXT_NAME]
+
+            return ",\n" \
+                .join(
+                    [TEXT_INPUT] \
+                        + list(
+                            map(
+                                get_text_argument,
+                                list_dicts_arguments)))
+
+        text_body = get_text_arguments() \
+            + "):\n\n" \
+            + get_text_python(list_dicts_body)
+
+        return "def " \
+            + TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
+            + text_name_function \
+            + "(\n" \
+            + m_common_functions.get_text_indented_one_level(text_body)
+
+    def get_text_python_memory_allocation(
+        dict_memory_allocation:typing.Dict):
+
+        text_key_memory = dict_memory_allocation \
+            [m_shared.Memory_allocation.KEY_TEXT_KEY_MEMORY]
+
+        dict_expression = dict_memory_allocation \
+            [m_shared.Memory_allocation.KEY_OBJECT_CONTENT]
+
+        return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
+            + text_key_memory \
+            + " = " \
+            + get_text_python_expression(dict_expression)
+
+    def get_text_python_return(
+        dict_return:typing.Dict):
+
+        return "return " \
+            + get_text_python_expression(
+                    dict_return 
+                        [m_shared.Expression_return.KEY_OBJECT])
+
+    def get_text_python_expression(
+        dict_expression:typing.Dict):
+
+        def get_text_literal(
+            dict_literal:typing.Dict):
+
+            return dict_literal \
+                [m_shared.Literal.KEY_TEXT_VALUE]
+
+        def get_text_memory_read(
+            dict_memory_read:typing.Dict):
+
+            return TEXT_PREFIX_TO_AVOID_NAME_CLASHES \
+                + dict_memory_read \
+                    [m_shared.Memory_read.KEY_TEXT_KEY_MEMORY]
+
+        def get_text_function(
+            dict_function:typing.Dict):
+
+            return "lambda " \
+                + TEXT_INPUT \
+                + ": " \
+                + get_text_python_function_call(
+                        text_input=TEXT_INPUT,
+                        dict_function=dict_function)
+
+        def get_text_initial(
+            dict_initial:typing.Dict):
+
+            text_category = dict_initial \
+                [m_shared.Object_variable.KEY_TEXT_CATEGORY]
+
+            return {
+                "literal": get_text_literal,
+                "memory_read": get_text_memory_read,
+                "function": get_text_function} \
+                [text_category] \
+                (dict_initial)
+
+        dict_initial = dict_expression \
+            [m_shared.Expression.KEY_OBJECT_INITIAL]
+
+        list_dicts_function_calls = dict_expression \
+            [m_shared.Expression.KEY_ARRAY_OBJECTS_FUNCTION_CALLS]
+
+        text_python_current = get_text_initial(dict_initial)
+
+        for dict_function in list_dicts_function_calls:
+            text_python_current = get_text_python_function_call(
+                    text_input=text_python_current,
+                    dict_function=dict_function)
+
+        return text_python_current
 
     text_category_block = dict_block \
         [m_shared.Object_variable.KEY_TEXT_CATEGORY]
